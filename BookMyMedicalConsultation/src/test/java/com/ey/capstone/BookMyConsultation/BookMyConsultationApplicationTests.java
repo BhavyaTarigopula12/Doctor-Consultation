42c8ca3ae9eb7a5fccfcc05c60e83dfb7ce35729
Jayashri Hamalatha G <jayashri.hamalatha.g@gds.ey.com>
2026-02-11 11:36:39 +0530

    Initial Commit

---
+++ BookMyMedicalConsultation/src/test/java/com/ey/capstone/BookMyConsultation/BookMyConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.capstone.BookMyConsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
